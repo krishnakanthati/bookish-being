e9c8b4cd7c344740f6839316b33e9a3a9f0d61d5
krishnakanthati <krishnakanthati@outlook.com>
2020-12-26 03:50:51 +0530

    ALLOWED_HOSTS

--- bookishbeing/settings.py
+++ bookishbeing/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '6^^)i!mlb=0au02rouo$f2fd^aa*=!0y0dicr_^ynsoe=^93)0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookish-being.herokuapp.com']
 
 
 # Application definition
