6daf9c2294f4ead3fee6ae50e33d44b697f699f9
krishnakanthati <krishnakanthati@outlook.com>
2020-12-26 03:36:18 +0530

    initial commit

---
+++ bookishbeing/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bookishbeing project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookishbeing.settings')
+
+application = get_asgi_application()
